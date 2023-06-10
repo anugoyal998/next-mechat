154b237ee3aa8ec3f3ec7f68e206a5017d403ecf
anugoyal998 <anugoyal998@gmail.com>
2023-06-10 13:23:22 +0530

    infinite scroll - usersidebar

--- src/components/UsersSidebar.tsx
+++ src/components/UsersSidebar.tsx
@@ -1,66 +1,62 @@
 "use client";
 
 import {
-  Dispatch,
-  FC,
-  SetStateAction,
   useContext,
   useState,
-  useEffect
+  useRef,
+  useCallback,
+  forwardRef,
 } from "react";
 import Input from "./ui/Input";
-import { Loader2, Search } from "lucide-react";
-import { GetUserTextResponseType } from "@/types/api.types";
-import Image from "next/image";
-import { getImageUrl } from "@/lib/utils";
 import { Skeleton } from "./ui/Skeleton";
-import {
-  ScrollAreaRoot,
-  ScrollAreaViewport,
-  ScrollAreaScrollbar,
-  ScrollAreaThumb,
-  ScrollAreaCorner,
-} from "@/components/ui/ScrollArea";
 import Text from "./ui/Text";
 import Link from "next/link";
+import * as Tooltip from "@/components/ui/Tooltip"
+import Image from "next/image";
+
+import { Loader2, Search } from "lucide-react";
+
+import { GetUserTextResponseType } from "@/types/api.types";
+import { Session } from "next-auth";
 import { RouteContext } from "./Route";
 
-interface UsersSidebarProps {
-  text: string;
-  setText: Dispatch<SetStateAction<string>>;
-  isLoading: boolean;
-  setIsLoading: Dispatch<SetStateAction<boolean>>;
-  users: GetUserTextResponseType[];
-  setUsers: Dispatch<SetStateAction<GetUserTextResponseType[]>>;
-}
+import { getImageUrl } from "@/lib/utils";
+import useInfiniteScroll from "@/hooks/useInfiniteScroll";
 
 const UsersSidebar = () => {
   const session = useContext(RouteContext);
-  const [text, setText] = useState('')
-  const [cursor, setCursor] = useState('$')
-  const [newCursor, setNewCursor] = useState('$')
-  const [isLoading, setIsLoading] = useState(false)
-  const [users, setUsers] = useState<GetUserTextResponseType[]>([])
+  const [text, setText] = useState("");
+  const [cursor, setCursor] = useState("$");
+  const {
+    isLoading,
+    hasMore,
+    data: users,
+    newCursor,
+  } = useInfiniteScroll(text, cursor);
 
-  useEffect(() => {
-    const getData = setTimeout(async () => {
-      try {
-        setIsLoading(true)
-        const { data } = await fetch(
-          `/api/get-user/${text ? text : "*"}/${cursor}`
-        ).then((res) => res.json());
-        const userData = data as GetUserTextResponseType[];
-        setUsers((prev) => [...prev, ...userData]);
-      } catch (err) {} finally { setIsLoading(false) }
-    },500)
-
-    return () => clearTimeout(getData);
-  },[text])
-
+  const observer = useRef();
+  /**@ts-ignore */
+  const lastUserElement = useCallback(
+    /**@ts-ignore */
+    (node) => {
+      if (isLoading) return;
+      /**@ts-ignore */
+      if (observer.current) observer.current.disconnect();
+      /**@ts-ignore */
+      observer.current = new IntersectionObserver((entries) => {
+        if (entries[0].isIntersecting && hasMore) {
+          setCursor(newCursor);
+        }
+      });
+      /**@ts-ignore */
+      if (node) observer.current.observe(node);
+    },
+    [isLoading, hasMore]
+  );
 
   return (
     <div
-      className="fixed w-[250px] bg-white border-r border-slate-300 shadow-sm"
+      className="fixed w-[250px] bg-white border-r border-slate-300 shadow-sm overflow-y-scroll"
       style={{ height: "calc(100vh - 5rem)" }}
     >
       <Input
@@ -78,95 +74,93 @@ const UsersSidebar = () => {
         onChange={(e) => {
           setText(e.target.value);
           setCursor("$");
-          setNewCursor("$");
-          setUsers([]);
         }}
       />
-      <ScrollAreaRoot className="w-full h-full">
-        <ScrollAreaViewport>
-          <div className="flex flex-col space-y-3 mt-1 px-3">
-            {users.length > 0 &&
-              session?.user?.email &&
-              users.map((user, index) =>
-                session?.user?.email !== user.email ? (
-                  index === users.length - 1 ? (
-                    <Link
-                      key={`${user.id}`}
-                      href={`/chat/${session.user?.email}/${user.email}`}
-                       /**@ts-ignore */
-                      // ref={lastUserElement}
-                    >
-                      <div className="border p-2 rounded-md flex space-x-2 cursor-pointer hover:bg-secondary">
-                        <Image
-                          src={getImageUrl(user.image, user.email)}
-                          alt="avatar"
-                          width={48}
-                          height={48}
-                          className="rounded-full"
-                        />
-                        <div className="flex-1 flex flex-col justify-center overflow-hidden">
-                          <Text className="truncate font-bold">
-                            {user.email}
-                          </Text>
-                          <Text className="truncate capitalize" size="sm">
-                            {user.name}
-                          </Text>
-                        </div>
-                      </div>
-                    </Link>
-                  ) : (
-                    <Link
-                      key={`${user.id}`}
-                      href={`/chat/${session.user?.email}/${user.email}`}
-                    >
-                      <div className="border p-2 rounded-md flex space-x-2 cursor-pointer hover:bg-secondary">
-                        <Image
-                          src={getImageUrl(user.image, user.email)}
-                          alt="avatar"
-                          width={48}
-                          height={48}
-                          className="rounded-full"
-                        />
-                        <div className="flex-1 flex flex-col justify-center overflow-hidden">
-                          <Text className="truncate font-bold">
-                            {user.email}
-                          </Text>
-                          <Text className="truncate capitalize" size="sm">
-                            {user.name}
-                          </Text>
-                        </div>
-                      </div>
-                    </Link>
-                  )
-                ) : null
-              )}
-            {users.length === 0 &&
-              Array.from({ length: 10 })
-                .map((_, i) => i + 1)
-                .map((ele) => (
-                  <div
-                    key={ele.toString()}
-                    className="flex items-center justify-between space-x-3 border p-2 rounded-md"
-                  >
-                    <Skeleton
-                      key={ele.toString()}
-                      className="h-14 w-14 rounded-full"
-                    />
-                    <div className="flex-1 flex flex-col space-y-2">
-                      <Skeleton className="h-2 w-full" />
-                      <Skeleton className="h-2 w-1/2" />
-                    </div>
-                  </div>
-                ))}
-          </div>
-        </ScrollAreaViewport>
-        <ScrollAreaScrollbar>
-          <ScrollAreaThumb />
-        </ScrollAreaScrollbar>
-        <ScrollAreaCorner />
-      </ScrollAreaRoot>
+      <div className="flex flex-col space-y-3 mt-1 px-3">
+        {users.length > 0 &&
+          session?.user?.email &&
+          users.map((user, index) =>
+            session?.user?.email !== user.email ? (
+              index === users.length - 1 ? (
+                <UserCard
+                  key={`${user.id}`}
+                  user={user}
+                  session={session}
+                  ref={lastUserElement}
+                />
+              ) : (
+                <UserCard key={`${user.id}`} user={user} session={session} />
+              )
+            ) : null
+          )}
+        {users.length === 0 && <SkeletonComponent />}
+      </div>
     </div>
   );
 };
 
 export default UsersSidebar;
+
+const SkeletonComponent = () => {
+  return (
+    <>
+      {Array.from({ length: 10 })
+        .map((_, i) => i + 1)
+        .map((ele) => (
+          <div
+            key={ele.toString()}
+            className="flex items-center justify-between space-x-3 border p-2 rounded-md"
+          >
+            <Skeleton key={ele.toString()} className="h-14 w-14 rounded-full" />
+            <div className="flex-1 flex flex-col space-y-2">
+              <Skeleton className="h-2 w-full" />
+              <Skeleton className="h-2 w-1/2" />
+            </div>
+          </div>
+        ))}
+    </>
+  );
+};
+
+interface UserCardProps {
+  user: GetUserTextResponseType;
+  session: Session | null;
+}
+
+const UserCard = forwardRef(({ user, session }: UserCardProps, ref) => {
+  return (
+    <Link
+      href={`/chat/${session?.user?.email}/${user.email}`}
+      /**@ts-ignore */
+      ref={ref}
+    >
+      <div className="border p-2 rounded-md flex space-x-2 cursor-pointer hover:bg-secondary">
+        <Image
+          src={getImageUrl(user.image, user.email)}
+          alt="avatar"
+          width={48}
+          height={48}
+          className="rounded-full"
+        />
+        <div className="flex-1 flex flex-col justify-center overflow-hidden">
+          <Tooltip.TooltipProvider>
+            <Tooltip.TooltipRoot>
+              <Tooltip.TooltipTrigger asChild>
+                <Text className="truncate font-bold">{user.email}</Text>
+              </Tooltip.TooltipTrigger>
+              <Tooltip.TooltipPortal>
+                <Tooltip.TooltipContent>
+                  {user.email}
+                  <Tooltip.TooltipArrow />
+                </Tooltip.TooltipContent>
+              </Tooltip.TooltipPortal>
+            </Tooltip.TooltipRoot>
+          </Tooltip.TooltipProvider>
+          <Text className="truncate capitalize" size="sm">
+            {user.name}
+          </Text>
+        </div>
+      </div>
+    </Link>
+  );
+});
